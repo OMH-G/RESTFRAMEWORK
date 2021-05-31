9b9ed5eaf3d874f3c908f76590f770d58d14f99c
OMH-G <70062727+OMH-G@users.noreply.github.com>
2021-05-31 12:38:15 +0530

    Delete Stream/Streamer directory

--- Stream/Streamer/StreamApp/serializers.py
+++
@@ -1,8 +0,0 @@
-from rest_framework import serializers
-from . models import StreamerName,Video
-
-class JStreamerName(serializers.ModelSerializer):
-    v_obj=serializers.StringRelatedField(many=True)
-    class Meta:
-        model=StreamerName 
-        fields=['name','quality','v_obj']
